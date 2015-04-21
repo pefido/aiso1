eb8f64b744b9319c9fbc5c7817f76d8706721727
pefido <pedrofilipeduraes@hotmail.com>
2015-04-21 18:42:56 +0100

    começando o timer

--- src/main/java/aiso/sim/hardware/Timer.java
+++ src/main/java/aiso/sim/hardware/Timer.java
@@ -1,16 +1,30 @@
 package aiso.sim.hardware;
 
+import aiso.sim.instructions.Instruction;
+
 
 public class Timer extends Interrupter implements Clockable {
+  
+  /**
+   * tempo definido para o timer lançar um interrupt(em ticks)
+   */
+  private static final int TIMER_TIME = 100;
 
-	@Override
-	public void tick() {
-		
-	}
+  @Override
+  public void tick() {
+    int timer = TIMER_TIME;
+    while(true){
+      if(timer == 1){
+        //lancar interrupt
+        timer = TIMER_TIME;
+      }
+      timer--;
+    }
+  }
 
-	@Override
-	public String getDescription() {
-		return "Timer";
-	}
+  @Override
+  public String getDescription() {
+    return "Timer";
+  }
 
 }
